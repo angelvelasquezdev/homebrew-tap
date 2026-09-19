366ce0ea306f8271cef9a0259c7b8ef700022add
Codemagic CI <ci@codemagic.io>
2026-09-19 02:45:30 +0000

    chore(cask): update macmirror to v1.0.1

--- Casks/macmirror.rb
+++ Casks/macmirror.rb
@@ -1,6 +1,6 @@
 cask "macmirror" do
-  version "1.0.0"
-  sha256 "185e805d08bda546859ad1c90a563c5af6ca05f0a98e4eae3372197666d90fb1"
+  version "1.0.1"
+  sha256 "87405121c2deaf6010621c387e33da731bad2ab123a458e9c185b85cd3b0c5e0"
 
   url "https://github.com/angelvelasquezdev/mac-mirror-macos/releases/download/v#{version}/MacMirror-v#{version}.dmg"
   name "MacMirror"
