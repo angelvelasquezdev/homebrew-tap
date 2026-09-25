6cf3dc04dc2d1cf5cf7e4a4cd625a7560bf14ebe
Codemagic CI <ci@codemagic.io>
2026-09-25 00:25:41 +0000

    chore(cask): update macmirror@dev to dev.20260925.66b7745

--- Casks/macmirror@dev.rb
+++ Casks/macmirror@dev.rb
@@ -1,6 +1,6 @@
 cask "macmirror@dev" do
-  version "dev.20260917.94342f4"
-  sha256 "3d47b1a1ea11b0d765f123711dc54134cee09114245e027d0f9c2d39bc6c287a"
+  version "dev.20260925.66b7745"
+  sha256 "7df6d54c6bc55ad20530ff33e5b276e83da7612c542da0417bdcd7c17d9fe7e7"
 
   url "https://github.com/angelvelasquezdev/mac-mirror-macos/releases/download/dev/MacMirror-dev.dmg"
   name "MacMirror (Dev)"
